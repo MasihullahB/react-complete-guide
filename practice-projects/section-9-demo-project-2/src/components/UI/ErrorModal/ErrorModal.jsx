e5ca83c2c31561b785f78f7c268bb5fb630b29e4
Masihullah Budye <masihullah.budye@outlook.com>
2023-07-26 19:30:41 +0530

    section 10 finished

--- practice-projects/section-9-demo-project-2/src/components/UI/ErrorModal/ErrorModal.jsx
+++ practice-projects/section-9-demo-project-2/src/components/UI/ErrorModal/ErrorModal.jsx
@@ -1,21 +1,43 @@
+import ReactDOM from 'react-dom';
 import Card from '../Card/Card';
 import Button from '../Button/Button';
 import styles from './ErrorModal.module.css';
+
+const Backdrop = ({ onErrorClose }) => {
+  return <div className={styles.backdrop} onClick={onErrorClose} />;
+};
+
+const ModalOverlay = ({ onErrorClose, title, message }) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={onErrorClose}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = ({ onErrorClose, title, message }) => {
   return (
     <>
-      <div className={styles.backdrop} onClick={onErrorClose} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={onErrorClose}>Okay</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onErrorClose={onErrorClose} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          onErrorClose={onErrorClose}
+          title={title}
+          message={message}
+        />,
+        document.getElementById('overlay-root')
+      )}
     </>
   );
 };
