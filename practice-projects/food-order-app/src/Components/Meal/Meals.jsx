8cd44f7447d42ef3cac0f2106cab0ba58a768158
Masihullah Budye <masihullah.budye@outlook.com>
2023-07-28 01:13:58 +0530

    app UI finised, now working on cart context.

---
+++ practice-projects/food-order-app/src/Components/Meal/Meals.jsx
@@ -0,0 +1,11 @@
+import Content from './Content/Content';
+import AvailableMeals from './AvailableMeals/AvailableMeals';
+const Meals = () => {
+  return (
+    <>
+      <Content />
+      <AvailableMeals />
+    </>
+  );
+};
+export default Meals;
